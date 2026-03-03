f82acc8a1529116c22f7da5255db03b9b8194374
Francesko Kola <francesko.kola@dev4side.com>
2026-03-03 11:30:11 +0100

    Hid google map api key and created app.config.js

---
+++ mobile/app.config.js
@@ -0,0 +1,12 @@
+export default ({ config }) => ({
+  ...config,
+  android: {
+    ...config.android,
+    config: {
+      ...(config.android?.config ?? {}),
+      googleMaps: {
+        apiKey: process.env.GOOGLE_MAPS_API_KEY
+      }
+    }
+  }
+});

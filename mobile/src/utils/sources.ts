a987579859bf8b61853af498b19b8a22a1fbedc8
Francesko Kola <francesko.kola@dev4side.com>
2026-03-06 10:27:48 +0100

    Changed sources page

--- mobile/src/utils/sources.ts
+++ mobile/src/utils/sources.ts
@@ -1,8 +1,7 @@
 export const DATA_SOURCES = {
   municipality: "https://tirana.al/pikat-e-interesit/linjat-urbane",
   portal: "https://urbani.tirana.al/en",
-  sourcesPage: "https://github.com/PhoenixKola/albania-transport/blob/main/docs/index.html",
-  // rawBase: "https://raw.githubusercontent.com/PhoenixKola/albania-transport/main/data",
+  sourcesPage: "https://phoenixkola.github.io/albania-transport/",
   gtfsZip: "https://pt.tirana.al/gtfs/gtfs.zip",
   disclaimerEn:
     "This is an independent app and is not affiliated with, endorsed by, or an official product of any government entity.",
